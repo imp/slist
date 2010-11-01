fb49e4210201636c4613ce9d736fe4b196cd88b9
Cyril Plisko <cyril.plisko@grigale.com>
2010-11-01 22:46:49 +0200

    Print func_id in hexa

--- iscsitgtd/isns.c
+++ iscsitgtd/isns.c
@@ -585,7 +585,7 @@ isns_send(int so, isns_pdu_t *pdu)
 
 	assert(pdu != NULL);
 
-	syslog(LOG_DEBUG, "isns_send(%d)", BE_16(pdu->func_id));
+	syslog(LOG_DEBUG, "isns_send(0x%04X)", BE_16(pdu->func_id));
 	len = ISNSP_HEADER_SIZE + ntohs(pdu->payload_len);
 	if (send(so, pdu, len, 0) == -1) {
 		syslog(LOG_ALERT, "isns_send failure");
@@ -700,7 +700,7 @@ isns_recv(int so, isns_rsp_t **pdu)
 	rsp->pdu_len = htons(total_pdu_len);
 	ntoh_isns_hdr((isns_hdr_t *)rsp);
 
-	syslog(LOG_DEBUG, "isns_recv(%d)", rsp->func_id);
+	syslog(LOG_DEBUG, "isns_recv(0x%04X)", rsp->func_id);
 
 	return (0);
 }
